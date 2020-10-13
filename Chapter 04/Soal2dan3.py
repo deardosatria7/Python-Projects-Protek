0951b1a177d5ce9d796e465377cb3df3f6e850d4
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-10-13 14:40:57 +0700

    Create Soal2dan3.py

---
+++ Chapter 04/Soal2dan3.py
@@ -0,0 +1,16 @@
+import time
+# Soal nomor 2
+
+jarakTempuh = a = 795
+jarakSatuLiterBBM = b = 12
+c = bahanBakar = a/b
+print ("Pak Budi menempuh jarak", a, "kilometer.")
+print ("Konsumsi bahan bakar mobil Pak Budi", b, "kilometer per liter")
+print ("Maka, Pak Budi membutuhkan", c, "liter bahan bakar, untuk sampai tujuan.")
+
+# Soal nomor 3
+time.sleep(2)
+print ("Jika kapasitas tangki maksimal mobil Pak Budi adalah 50 liter,")
+d = kapasitasMax = 50
+print ("Maka Pak Budi harus mengisi bensin setidaknya", int(c//d), "kali.")
+print ("(Dengan kondisi awal mobil Pak Budi terisi penuh)")
