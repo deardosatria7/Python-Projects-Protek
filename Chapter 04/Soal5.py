0f7dd36b7b485700f7f80278ffa6a2cb03497b98
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-10-17 19:20:29 +0700

    Update Soal5.py

--- Chapter 04/Soal5.py
+++ Chapter 04/Soal5.py
@@ -1,5 +1,5 @@
 # ini soal nomor 5
-print ("Grafik horiontal jumlah mahasiswa laki-laki dan perempuan")
+print ("Grafik horizontal jumlah mahasiswa laki-laki dan perempuan")
 jmlLakiLaki = a = int (input ("Silahkan masukkan data jumlah mahasiswa laki-laki : "))
 jmlPerempuan = b = int (input ("Silahkan masukkan data jumlah mahasiswa perempuan : "))
 c = int (jmlLakiLaki/10)
