87f3c0c2505957dd2568d0a11a97bcef674f61ba
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-10-21 21:15:35 +0700

    Create nomor1.py

---
+++ Chapter 05/Latihan 1/nomor1.py
@@ -0,0 +1,17 @@
+nIndo = int( input ("Silahkan masukkan nilai Bahasa Indonesia     :"))
+nMat = int( input ("Silahkan masukkan nilai Matematika           :"))
+nIpa = int( input ("Silahkan masukkan nilai IPA                  :"))
+if (nMat > 70) & (nIpa > 60) & (nIndo > 60):
+    print ("Status kelulusan                             :LULUS")
+else:
+    print ("Status kelulusan                             :TIDAK LULUS")
+
+    
+
+
+
+
+
+
+
+
