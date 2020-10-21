d34efa82b4252f3797a7ad4be6274c209baca45c
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-10-21 21:15:45 +0700

    Create nomor2.py

---
+++ Chapter 05/Latihan 1/nomor2.py
@@ -0,0 +1,15 @@
+nIndo = int( input ("Silahkan masukkan nilai Bahasa Indonesia           :"))
+nMat = int( input ("Silahkan masukkan nilai Matematika                 :"))
+nIpa = int( input ("Silahkan masukkan nilai IPA                        :"))
+if (nIndo < 0 or nIndo > 100) : 
+    print ("Maaf, nilai yang anda masukkan ada yang tidak valid (0-100)")
+elif (nMat < 0 or nMat > 100) :
+    print ("Maaf, nilai yang anda masukkan ada yang tidak valid (0-100)")
+elif (nIpa < 0 or nIpa > 100):
+    print ("Maaf, nilai yang anda masukkan ada yang tidak valid (0-100)")
+elif (nMat > 70 and nMat <= 100) & (nIpa > 60 and nIpa <= 100 and nIpa >= 0) & (nIndo > 60 and nIndo <= 100 and nIndo >= 0):
+    print ("Status kelulusan                                   :LULUS")
+elif (nMat < 70 and nMat >= 0) or (nIpa < 60 and nIpa >= 0) or (nIndo < 60 and nIndo >= 0):
+    print ("Status kelulusan                                   :TIDAK LULUS")
+
+    
