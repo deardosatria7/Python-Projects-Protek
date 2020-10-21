cad9053782d4146e1af600bfb6c911aaa49d40fa
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-10-21 21:16:39 +0700

    Create nomor6.py

---
+++ Chapter 05/Latihan 2/nomor6.py
@@ -0,0 +1,27 @@
+from random import randint
+bilTebak = randint (0,100)
+sum = 0
+print ("Halo... Nama saya Mr. Komputer :D")
+print ("Saya telah memilih bilangan secara acak dari 0 - 100")
+print ("Coba tebak, bilangan berapakah itu?? :)))")
+while True :
+    bilAnda = int ( input ("Silahkan masukkan tebakkan Anda :"))
+
+    if (bilAnda > 0) and (bilAnda < bilTebak):
+        print ("Ups... Kurang tepat, bilangan Anda terlalu kecil")
+        sum += 1
+    elif (bilAnda < 100) and (bilAnda > bilTebak):
+        print ("Yahhh... Masih salah, bilangan Anda terlalu besar")
+        sum += 1
+        continue
+    elif (bilAnda == bilTebak):
+        print ("Selamatttt tebakan Anda tepat :>")
+        score = 100 - sum * 2
+        if ( score > 0):
+            print ("Score Anda = ", score)
+        else :
+            print ("Yahhh... Maaf score Anda nol, Anda gagal :(((")
+        break
+    else :
+        print ("Ingat yaa, bilangan 0 - 100 :D ")
+        continue
