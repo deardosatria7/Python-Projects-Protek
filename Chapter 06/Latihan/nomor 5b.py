c4a76b5fb24f00d83abf1f79e3891ac55b66ced5
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-10-29 21:51:32 +0700

    Create nomor 5b.py

---
+++ Chapter 06/Latihan/nomor 5b.py
@@ -0,0 +1,8 @@
+from statistik import *
+
+print ("Nomor 5b")
+print ("Data : 81, 98, 12, 83, 45, 77, 69, 30, 56")
+print ("Jumlah seluruh data diatas adalah ", sum1(81, 98, 12, 83, 45, 77, 69, 30, 56))
+print ("Rata-ratanya adalah ", average(81, 98, 12, 83, 45, 77, 69, 30, 56))
+print ("Nilai maksimum dari data diatas adalah ", maksimum(81, 98, 12, 83, 45, 77, 69, 30, 56))
+print ("Nilai minimumnya adalah ", minimum(81, 98, 12, 83, 45, 77, 69, 30, 56))
