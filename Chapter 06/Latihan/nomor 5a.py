b6204f1a0c3211fc802aa4d1e71a72f26c9a9a27
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-10-29 21:51:27 +0700

    Create nomor 5a.py

---
+++ Chapter 06/Latihan/nomor 5a.py
@@ -0,0 +1,8 @@
+from statistik import *
+
+print ("Nomor 5a")
+print ("Data : 5, 10, 4, 9, 30, 16, 2, 11")
+print ("Jumlah seluruh data diatas adalah ", sum1(5, 10, 4, 9, 30, 16, 2, 11))
+print ("Rata-ratanya adalah ", average(5, 10, 4, 9, 30, 16, 2, 11))
+print ("Nilai maksimum dari data diatas adalah ", maksimum(5, 10, 4, 9, 30, 16, 2, 11))
+print ("Nilai minimumnya adalah ", minimum(5, 10, 4, 9, 30, 16, 2, 11))
