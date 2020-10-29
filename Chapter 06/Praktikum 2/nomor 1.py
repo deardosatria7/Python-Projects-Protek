95d89a1f38223d04a52c0f27beb46ba5f69ca6c1
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-10-29 21:51:52 +0700

    Create nomor 1.py

---
+++ Chapter 06/Praktikum 2/nomor 1.py
@@ -0,0 +1,9 @@
+def luasSegitiga (a, t):
+    luas = a * t / 2
+    return luas
+
+alas = a = 10
+tinggi = t = 20
+print ('Luas segitiga dengan alas ', alas,
+       ' dan tinggi ', tinggi,
+       ' adalah ', luasSegitiga(a, t))
