c5421be9abc2bea6552db2d9187e90d3e3a7aca6
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-11-28 10:20:43 +0700

    Create nomor 9.py

---
+++ Chapter 08/Project/nomor 9.py
@@ -0,0 +1,20 @@
+buah = {'apel' : 5000, 'jeruk' : 8500, 'mangga' : 7800, 'duku' : 6500}
+print ("-----Program penjual buah-----\n")
+print ("Daftar harga buah per kilogram")
+print (buah, "\n")
+while True:
+    beli = input ("Nama buah yang ingin dibeli : ")
+    if beli in buah:
+        try:
+            kilo = int ( input ("jumlah kilogram             : "))
+            harga = kilo * buah[beli]
+            print("-------------------------------")
+            print("Total harga                 : Rp", harga)
+            break
+        except (ValueError):
+            print ("Masukkan jumlah kilogram yang valid")
+            break
+    elif (beli == '') or (beli not in buah):
+        print ("Buah yang ingin Anda beli tidak ada di daftar")
+    
+    
