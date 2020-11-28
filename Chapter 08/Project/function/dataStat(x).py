302eaec4bbee4746b578adc43f4165f9b4cdccee
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-11-28 10:20:01 +0700

    Create dataStat(x).py

---
+++ Chapter 08/Project/function/dataStat(x).py
@@ -0,0 +1,10 @@
+def dataStat (x):
+    d = []
+    for data in x :
+        a = sum(x)/len(x)
+        b = max(x)
+        c = min(x)
+    d.append(a)
+    d.append(b)
+    d.append(c)
+    print (d)
