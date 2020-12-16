193870f699ad8a3a43e71a0bc84f270625316185
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-12-16 23:45:20 +0700

    Create function ubahHuruf.py

---
+++ Chapter 09/function ubahHuruf.py
@@ -0,0 +1,11 @@
+def ubah_huruf (kata, a, b):
+    kata_list = list (kata)
+    if a in kata_list :
+        while a in kata_list :
+            kata_list[kata_list.index(a)] = b
+        kata = ''.join(kata_list)
+        print (kata)
+    elif a not in kata_list:
+        print ("Huruf",a,"tidak ada dalam kata",kata)
+     
+
