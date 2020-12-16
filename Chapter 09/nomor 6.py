4a75609646dfe8b07edd88cca4f90faa5a6c17fd
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-12-16 23:45:36 +0700

    Create nomor 6.py

---
+++ Chapter 09/nomor 6.py
@@ -0,0 +1,27 @@
+nilai = [{'nim' : 'A01', 'nama' : 'Agustina', 'mid' : 50, 'uas' : 80},
+         {'nim' : 'A02', 'nama' : 'Budi', 'mid' : 40, 'uas' : 90}, 
+         {'nim' : 'A03', 'nama' : 'Chicha', 'mid' : 100, 'uas' : 50}, 
+         {'nim' : 'A04', 'nama' : 'Donna', 'mid' : 20, 'uas' : 100},
+         {'nim' : 'A05', 'nama' : 'Fatimah', 'mid' : 70, 'uas' : 100}]
+
+print ('=' * 90)
+print ("NIM", "\t\t NAMA", "\t\tN.MID", "\t\tN.UAS", "\t\tN.AKHIR", "\tSTATUS")
+print ('-' * 90)
+
+for data in nilai :
+    print (data['nim'],"\t\t", end ='')
+    if (len(data['nama']) > 6) :
+        print (data['nama'],"\t", end ='')
+    else :
+        print (data['nama'],"\t\t", end='')
+    print (data['mid'],"\t\t",end='')
+    print (data['uas'],"\t\t", end='')
+    nA = (data['mid'] + (2*data['uas']))/3
+    nA_bulat = round(nA , 2)
+    if (nA_bulat >= 60):
+        status = 'LULUS'
+    else :
+        status = 'GAGAL'
+    print (nA_bulat,"\t\t",status)
+
+print ('-' * 90)
