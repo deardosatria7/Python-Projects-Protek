2e109dceccb52c68f5485fdd9f8a18117326008c
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-12-17 16:14:28 +0700

    Create nomor 4.py

---
+++ Chapter 09/nomor 4.py
@@ -0,0 +1,14 @@
+import random
+import sys
+def shuffleString (kata, n):
+    memori = []
+    while (len(memori) < n):
+        i = random.sample(kata, len(kata))
+        hasil = ''.join(i)
+        if hasil not in memori :
+            memori.append(hasil)
+
+    print (memori)
+
+
+shuffleString('aku', 4)
