f7d1e264a00a9383e020878c689d8fbdf6a600d6
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-12-24 14:44:25 +0700

    Create nomor 1.py

---
+++ Chapter 10/nomor 1.py
@@ -0,0 +1,23 @@
+alamat = input ("Masukkan alamat dari file yang ingin dibuka (contoh = D:\Folder\File) : ")
+try:
+    file = open(alamat, 'r' )
+    genap = []
+    ganjil = []
+    data_int = []
+    try:
+        for data in file:
+            data_int.append(int(data))
+        print()
+        for data in data_int:
+            print (data)
+            if (data % 2 == 0):
+                genap.append(data)
+            else :
+                ganjil.append(data)
+        print ("\nTotal bilangan genap ada",len(genap))
+        print ("Total bilangan ganjil ada",len(ganjil))
+    except (ValueError):
+        print ("\nIsi text harus berupa bilangan!")
+except (OSError):
+    print("\nFile tidak ditemukan")
+
