6133ebd5ea8a2f4f757b3ccc1c78e933bc2270f5
deardosatria7 <71997409+deardosatria7@users.noreply.github.com>
2020-12-27 01:31:59 +0700

    Update nomor 1.py

--- Chapter 10/nomor 1.py
+++ Chapter 10/nomor 1.py
@@ -16,8 +16,10 @@ try:
                 ganjil.append(data)
         print ("\nTotal bilangan genap ada",len(genap))
         print ("Total bilangan ganjil ada",len(ganjil))
+        file.close()
     except (ValueError):
         print ("\nIsi text harus berupa bilangan!")
+        file.close()
 except (OSError):
     print("\nFile tidak ditemukan")
 
